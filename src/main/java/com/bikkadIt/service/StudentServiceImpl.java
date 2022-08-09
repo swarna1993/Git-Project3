e7a0391987b9803888f668116147dd50b11a7ab3
swarna1993 <swarna.krisanya@gmail.com>
2022-08-09 05:57:30 +0530

    SMS-03:StudentServiceImpl class created

---
+++ src/main/java/com/bikkadIt/service/StudentServiceImpl.java
@@ -0,0 +1,27 @@
+package com.bikkadIt.service;
+
+import java.util.List;
+
+import com.bikkadIt.model.Student;
+
+public class StudentServiceImpl implements StudentServiceI{
+
+	@Override
+	public int addStudent(Student student) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Student> getAllstudent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student getStudentbyId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
