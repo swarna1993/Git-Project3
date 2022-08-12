cd5479a9a58f7a4fa70901680459a0800ead047d
swarna1993 <swarna.krisanya@gmail.com>
2022-08-13 03:02:22 +0530

    service bean updated

--- src/main/java/com/bikkadIt/service/StudentServiceImpl.java
+++ src/main/java/com/bikkadIt/service/StudentServiceImpl.java
@@ -3,10 +3,12 @@ package com.bikkadIt.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.bikkadIt.model.Student;
 import com.bikkadIt.repository.StudentRepository;
 
+@Service
 public class StudentServiceImpl implements StudentServiceI{
 	
 	@Autowired
